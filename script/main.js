c237304e2aaa93fcbc883f39583a865a00df6a75
Nevezman <nevezy4ii321@gmail.com>
2022-12-20 17:30:20 +0400

    change jobs block

--- script/main.js
+++ script/main.js
@@ -11,19 +11,4 @@ function onEntry(entry) {
   for (let elm of elements) {
     observer.observe(elm);
   }
-  $(document).ready(function() {
-
-	$("#form").submit(function() {
-		$.ajax({
-			type: "POST",
-			url: "mail.php",
-			data: $(this).serialize()
-		}).done(function() {
-			$(this).find("input").val("");
-			alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
-			$("#form").trigger("reset");
-		});
-		return false;
-	});
-	
-});
+  
