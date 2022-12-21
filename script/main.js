7173034e8476e3f12480452146cb2af0d85330e8
Nevezman <nevezy4ii321@gmail.com>
2022-12-21 21:50:21 +0400

    init modal window

--- script/main.js
+++ script/main.js
@@ -11,4 +11,21 @@ function onEntry(entry) {
   for (let elm of elements) {
     observer.observe(elm);
   }
-  
+
+let popup = document.querySelector('.popup'); // Само окно
+let openPopupButtons = document.querySelectorAll('.button'); // Кнопки для показа окна
+let closePopupButton = document.querySelector('.popup__area'); // Кнопка для скрытия окна
+
+openPopupButtons.forEach((button) => { // Перебираем все кнопки
+  button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
+      e.preventDefault(); // Предотвращаем дефолтное поведение браузера
+      popup.classList.add('active'); // И для самого окна
+      document.body.style.overflow = "hidden";
+      document.body.style.paddingRight= "15px"
+  })
+});
+closePopupButton.addEventListener('click',() => { // Вешаем обработчик на крестик
+  popup.classList.remove('active'); // И с окна
+  document.body.style.overflow = "";
+  document.body.style.paddingRight= ""
+});
